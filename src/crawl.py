d04aa643da1fe3affbab558e3003d3c0a121760b
j3arch <j3archx@gmail.com>
2026-05-04 23:38:49 +0300

    setup

--- src/crawl.py
+++ src/crawl.py
@@ -5,3 +5,9 @@ def normalize_url(url):
     full_path = f"{parsed_url.netloc}{parsed_url.path}"
     full_path = full_path.rstrip("/")
     return full_path.lower()
+
+def get_heading_from_html():
+    pass
+
+def get_urls_from_html():
+    pass
