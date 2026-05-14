1f5ef0a9dc2c8178856281e8d1325f6c56bfe4b8
j3arch <j3archx@gmail.com>
2026-05-14 23:21:46 +0300

    heading setup and library import

--- src/crawl.py
+++ src/crawl.py
@@ -1,10 +1,16 @@
 from urllib.parse import urlparse
+from bs4 import BeautifulSoup
 
 def normalize_url(url):
     parsed_url = urlparse(url)
     full_path = f"{parsed_url.netloc}{parsed_url.path}"
     full_path = full_path.rstrip("/")
     return full_path.lower()
+
+def get_first_paragraph_from_html(html):
+    pass
+
+
 
 def get_heading_from_html():
     pass
