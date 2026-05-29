e431d4533e3a6a7ddf2ab3e27527e772723a78b8
j3arch <j3archx@gmail.com>
2026-05-29 22:34:50 +0300

    tests update

--- src/test_crawl.py
+++ src/test_crawl.py
@@ -100,6 +100,13 @@ class TestCrawl(unittest.TestCase):
         expected = ["https://crawler-test.com/path/one", "https://other.com/path/one"]
         self.assertEqual(actual, expected)
 
+    def test_get_images_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
     
 
 if __name__ == "__main__":
