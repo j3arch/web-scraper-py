0c771d2cce7569eedea9acebfb3bdbfd029d20e0
j3arch <j3archx@gmail.com>
2026-05-30 02:21:21 +0300

    update

--- src/test_crawl.py
+++ src/test_crawl.py
@@ -4,7 +4,8 @@ from crawl import (
     get_first_paragraph_from_html,
     get_heading_from_html,
     get_urls_from_html,
-    get_images_from_html
+    get_images_from_html,
+    extract_page_data
 )
 
 
@@ -111,7 +112,25 @@ class TestCrawl(unittest.TestCase):
         self.assertEqual(actual, expected)
     
     
-    #
+    # test get data
+
+    def test_extract_page_data_basic(self):
+        input_url = "https://crawler-test.com"
+        input_body = '''<html><body>
+            <h1>Test Title</h1>
+            <p>This is the first paragraph.</p>
+            <a href="/link1">Link 1</a>
+            <img src="/image1.jpg" alt="Image 1">
+        </body></html>'''
+        actual = extract_page_data(input_body, input_url)
+        expected = {
+            "url": "https://crawler-test.com",
+            "heading": "Test Title",
+            "first_paragraph": "This is the first paragraph.",
+            "outgoing_links": ["https://crawler-test.com/link1"],
+            "image_urls": ["https://crawler-test.com/image1.jpg"]
+        }
+        self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
     unittest.main()
