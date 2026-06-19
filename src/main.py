d8e16df1bb408afcc5f21124f044cd54d3c991dc
j3arch <j3archx@gmail.com>
2026-06-19 23:14:11 +0300

    main update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import sys
+
 def main() -> None:
-    print("Hello from web-scraper-py!")
+    args = sys.argv
+    if len(args) < 2:
+        print("no website provided")
+        sys.exit(1)
+    if len(args) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+
+    base_url = args[1]
+    print(f"starting crawl of: {base_url}...")
+    # print("Hello from web-scraper-py!")
 
 
 if __name__ == "__main__":
