d94c8507187b5652d3a79e4352b0d86e02da9925
j3arch <j3archx@gmail.com>
2026-07-16 23:35:25 +0300

    report

--- src/json_report.py
+++ src/json_report.py
@@ -1,5 +1,7 @@
 import json
 from crawl import PageData
 
-def write_json_report(page_data: dict[str, PageData], filename: str ="report.json"):
-    pass
+def write_json_report(page_data: dict[str, PageData], filename: str ="report.json") -> None:
+    if not page_data:
+        print("No data to write to JSON")
+        return
