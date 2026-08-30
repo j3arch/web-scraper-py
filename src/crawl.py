71e5669c8db250e3846a2d299f2e968e1ac3eb4a
j3arch <j3archx@gmail.com>
2026-08-30 22:28:55 +0300

    test

--- src/crawl.py
+++ src/crawl.py
@@ -6,12 +6,14 @@ from urllib.parse import urljoin, urlsplit
 import aiohttp
 from bs4 import BeautifulSoup, Tag
 
+
 class PageData(TypedDict):
     url: str
     heading: str
     first_paragraph: str
-    outgoing_link: list[str]
-    image_links: list[str]
+    outgoing_links: list[str]
+    image_urls: list[str]
+
 
 def normalize_url(url: str) -> str:
     parsed_url = urlsplit(url)
@@ -19,21 +21,23 @@ def normalize_url(url: str) -> str:
     full_path = full_path.rstrip("/")
     return full_path.lower()
 
+
 def get_heading_from_html(html: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
     h_tag = soup.find("h1") or soup.find("h2")
-    return h_tag.get_text(strip=True) if h_tag else ""
+    return h_tag.get_text(strip=True) if isinstance(h_tag, Tag) else ""
+
 
 def get_first_paragraph_from_html(html: str) -> str:
     soup = BeautifulSoup(html, "html.parser")
 
     main_section = soup.find("main")
-    if main_section:
+    if isinstance(main_section, Tag):
         first_p = main_section.find("p")
     else:
         first_p = soup.find("p")
-    return first_p.get_text(strip=True) if first_p else ""
 
+    return first_p.get_text(strip=True) if isinstance(first_p, Tag) else ""
 
 
 def get_urls_from_html(html: str, base_url: str) -> list[str]:
@@ -51,7 +55,7 @@ def get_urls_from_html(html: str, base_url: str) -> list[str]:
                 urls.append(absolute_url)
             except Exception as e:
                 print(f"{str(e)}: {href}")
-    
+
     return urls
 
 
@@ -70,8 +74,9 @@ def get_images_from_html(html: str, base_url: str) -> list[str]:
                 image_urls.append(absolute_url)
             except Exception as e:
                 print(f"{str(e)}: {src}")
-    
+
     return image_urls
+
 
 def extract_page_data(html: str, page_url: str) -> PageData:
     return {
@@ -82,58 +87,6 @@ def extract_page_data(html: str, page_url: str) -> PageData:
         "image_urls": get_images_from_html(html, page_url),
     }
 
-'''def crawl_page(base_url: str, current_url: str| None = None, page_data: dict[str, PageData] | None = None) -> dict[str, PageData]:
-    if current_url is None:
-        current_url = base_url
-    if page_data is None:
-        page_data = {}
-
-    base_url_obj = urlparse(base_url)
-    current_url_obj = urlparse(current_url)
-    if current_url_obj.netloc != base_url_obj.netloc:
-        return page_data
-    
-    normalized_url = normalize_url(current_url)
-
-    if normalized_url in page_data:
-        return page_data
-    
-    print(f"crawling {current_url}")
-    html = safe_get_html(current_url)
-    if html is None:
-        return page_data
-    
-    page_info = extract_page_data(html, current_url)
-    page_data[normalized_url] = page_info
-
-    next_urls = get_urls_from_html(html, base_url)
-    for next_url in next_urls:
-        page_data = crawl_page(base_url, next_url, page_data)
-    
-    return page_data
-    
-
-def get_html(url: str) -> str:
-    try:
-        response = requests.get(url, headers={"User-Agent": "BootCrawler/1.0"})
-    except Exception as e:
-        raise Exception(f"network error while fetching {url}: {e}")
-
-    if response.status_code > 399:
-        raise Exception(f"got HTTP error: {response.status_code} {response.reason}")
-
-    content_type = response.headers.get("content-type", "")
-    if "text/html" not in content_type:
-        raise Exception(f"got non-HTML response: {content_type}")
-
-    return response.text
-
-def safe_get_html(url: str) -> str | None:
-    try:
-        return get_html(url)
-    except Exception as e:
-        print(f"{e}")
-        return None'''
 
 class AsyncCrawler:
     def __init__(self, base_url: str, max_concurrency: int, max_pages: int) -> None:
