4a18b2d4b12e06d38483eace248e3292798a4ffd
j3arch <j3archx@gmail.com>
2026-09-07 22:15:03 +0300

    typo fix

--- src/crawl.py
+++ src/crawl.py
@@ -2,7 +2,7 @@ from urllib.parse import urlsplit
 
 def normalize_url(url: str) -> str:
     parsed_url = urlsplit(url)
-    full_path = f"{parsed_url.netlock}{parsed_url.path}"
+    full_path = f"{parsed_url.netloc}{parsed_url.path}"
     full_path = full_path.rstrip("/")
     return full_path.lower()
 
