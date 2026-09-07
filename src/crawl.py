43220355087aeaa41c7bd25979eb08747ba413a5
j3arch <j3archx@gmail.com>
2026-09-07 22:13:11 +0300

    normalize url

--- src/crawl.py
+++ src/crawl.py
@@ -1,2 +1,8 @@
+from urllib.parse import urlsplit
 
+def normalize_url(url):
+    parsed_url = urlsplit(url)
+    full_path = f"{parsed_url.netlock}{parsed_url.path}"
+    full_path = full_path.rstrip("/")
+    return full_path.lower()
 
