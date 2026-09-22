9869dbc8abac922e3bbe836fd6f34be08e1237b1
j3arch <j3archx@gmail.com>
2026-09-22 03:59:11 +0300

    main update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys
-from crawl import get_html
+
+from crawl import crawl_page
+
 
 def main() -> None:
     args = sys.argv
@@ -14,17 +16,15 @@ def main() -> None:
 
     print(f"starting crawl of: {base_url}...")
 
-    try:
-        html = get_html(base_url)
-    except Exception as e:
-        print(f"Error fetching HTML from {base_url}: {str(e)}")
-        sys.exit(1)
+    page_data = crawl_page(base_url)
 
-    print(html)
-
+    print(f"Found {len(page_data)} pages:")
+    for page in page_data.values():
+        print(f"- {page['url']}: {len(page['outgoing_links'])} outgoing links")
 
     sys.exit(0)
 
 
 if __name__ == "__main__":
     main()
+
